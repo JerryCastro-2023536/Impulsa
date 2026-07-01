ed4e973ba4b4b774f0dfea56785f339c70ebcbb5
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-07-01 07:52:35 -0600

    feat: Se mepaeo la entidad de Usuario

---
+++ Sistema/src/models/Usuario.ts
@@ -0,0 +1,12 @@
+export interface Usuario{
+    id_usuario : number,
+    nombre : string,
+    apellido : string,
+    username : string,
+    correo : string,
+    paswword : string,
+    telefono : number,
+    fecha_registro : Date,
+    rol : string,
+    estado : string
+}
