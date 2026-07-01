c9bdf340a81d0e6f2e104c29fce82ce416e9e4dc
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-07-01 10:23:07 -0600

    feat: Se cargo el enum de los tipo de organizacion

---
+++ Sistema/src/enums/TipoOrganizacion.ts
@@ -0,0 +1,8 @@
+export enum TipoOrganizacion{
+    ONG = "ONG",
+    FUNDACION = "Fundación",
+    EMPRESA = "Empresa",
+    COOPERATIVA = "Cooperativa",
+    ASOCIACION = "Asociación",
+    INTERNACIONAL = "Internacional"
+}
