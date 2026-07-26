3a9a37c90eaf909f5577f4113d2b853c6a9350a0
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-07-25 19:23:56 -0600

    feat: Se agrego el server del proyecto

---
+++ Sistema/src/server.ts
@@ -0,0 +1,49 @@
+import * as http from "http";
+import { UsuarioRouter } from "./api/UsuarioRouter";
+import { TituloRouter } from "./api/TituloRouter";
+import { DocumentoRouter } from "./api/DocumentoRouter";
+import { ExperienciaRouter } from "./api/ExperienciaRouter";
+import { PerfilRouter } from "./api/PerfilRouter";
+import { OrganizacionRouter } from "./api/OrganizacionRouter";
+import { PostulacionRouter } from "./api/PostulacionRouter";
+import { RecomendacionRouter } from "./api/RecomendacionRouter";
+import * as dotev from "dotenv";
+import { testConexion } from "./data/Conexion";
+
+dotev.config();
+
+const port = process.env.PORT
+
+const server = http.createServer(async (req, res) => {
+    const handled =
+        await UsuarioRouter(req, res) ||
+        await TituloRouter(req, res) ||
+        await DocumentoRouter(req, res) ||
+        await ExperienciaRouter(req, res) ||
+        await PerfilRouter(req, res) ||
+        await OrganizacionRouter(req, res) ||
+        await PostulacionRouter(req, res) ||
+        await RecomendacionRouter(req, res);
+
+    if (!handled) {
+        res.writeHead(404, {
+            "Content-Type": "application/json"
+        });
+        res.end(JSON.stringify({ error: "Ruta no encontrada." }));
+    }
+});
+
+
+async function start(){
+
+    await testConexion();
+
+    server.listen(port, () =>{
+    console.log("------------------------------------------");
+    console.log("Servidor iniciado en http://localhost:" + port);
+    console.log("------------------------------------------");
+    });
+}
+
+
+start();
