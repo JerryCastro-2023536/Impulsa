36496a29e9b01dc28b344f1e775482f4d3bad797
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-07-27 21:31:09 -0600

    feat: Se agrego la validacion de Oportunidad

---
+++ Sistema/src/validators/OportunidadValidate.ts
@@ -0,0 +1,46 @@
+import { Oportunidad } from "../models/Oportunidad";
+import { Estado } from "../enums/Estado";
+import { isEmptyString, isInvalidNumber, isInvalidDate, isInvalidEnum } from "./validatorHelpers";
+
+export function OportunidadValidate(oportunidad: Oportunidad) {
+    const errores: string[] = [];
+
+    if (isEmptyString(oportunidad.titulo)) {
+        errores.push("titulo vacio");
+    }
+    if (isEmptyString(oportunidad.tipo)) {
+        errores.push("tipo vacio");
+    }
+    if (isEmptyString(oportunidad.categoria)) {
+        errores.push("categoria vacio");
+    }
+    if (isEmptyString(oportunidad.descripcion)) {
+        errores.push("descripcion vacio");
+    }
+    if (isEmptyString(oportunidad.requisitos)) {
+        errores.push("requisitos vacio");
+    }
+    if (isInvalidDate(oportunidad.fecha_publicacion)) {
+        errores.push("fecha_publicacion vacia o invalida");
+    }
+    if (isInvalidDate(oportunidad.fecha_limite)) {
+        errores.push("fecha_limite vacia o invalida");
+    }
+    if (isEmptyString(oportunidad.modalidad)) {
+        errores.push("modalidad vacio");
+    }
+    if (isEmptyString(oportunidad.ubicacion)) {
+        errores.push("ubicacion vacio");
+    }
+    if (oportunidad.foto === null || oportunidad.foto === undefined) {
+        errores.push("foto vacio");
+    }
+    if (isInvalidEnum(Estado, oportunidad.estado)) {
+        errores.push("estado vacio o invalido");
+    }
+    if (isInvalidNumber(oportunidad.id_organizacion)) {
+        errores.push("id_organizacion vacio o invalido");
+    }
+
+    return errores;
+}
